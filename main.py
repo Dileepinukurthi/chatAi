b94d4fe542e2dd09236363ecdb93b087de8e8e63
Inukurthi Dileep <88875532+Dileepinukurthi@users.noreply.github.com>
2023-06-08 11:44:11 +0530

    chat implemented with gpt 3.5 turbo model

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import os
 import json
 import openai
-openai.my_api_key=json.loads(open("keys.json").read())['openaikey']
+openai.api_key=json.loads(open("keys.json").read())['openaikey']
 messages=[{"role":"system","content":"intelligent system"}]
+while(True):
+    question=input("user: ").strip()
+    if messages:
+        messages.append({"role":"user","content":question})
+        chat=openai.ChatCompletion.create(model='gpt-3.5-turbo',messages=messages)
+    answer=chat.choices[0].message.content
+    print("AI: " + answer)
+messages=[{"role":"system","content":answer}]
+
+
 
 
